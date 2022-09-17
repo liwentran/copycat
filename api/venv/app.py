cb6f0204f9da78b93f42464d7c2a1e7c07a030ee
Ataes <ataes@aggarwals.com>
2022-09-16 23:50:36 -0400

    model for emotion recog on flask

--- api/venv/app.py
+++ api/venv/app.py
@@ -33,97 +33,26 @@ def home():
 #POST
 @app.route('/posting', methods=['POST'])
 def something():
+    from fer import FER
+    import matplotlib.pyplot as plt
+    import tensorflow
+
     file = request.files['image']
     img = Image.open(file.stream)
 
-    data_dir_train = pathlib.Path("/Users/ataesaggarwal/code/SCCAgain/Skin cancer ISIC The International Skin Imaging Collaboration/Train")
-    data_dir_test = pathlib.Path('/Users/ataesaggarwal/code/SCCAgain/Skin cancer ISIC The International Skin Imaging Collaboration/Test')
+    test_image_one = plt.imread(img)
+    emo_detector = FER(mtcnn=True)
+    # Capture all the emotions on the image
+    captured_emotions = emo_detector.detect_emotions(test_image_one)
+    # Print all captured emotions with the image
+    print(captured_emotions)
+    plt.imshow(test_image_one)
 
-    batch_size = 32
-    img_height = 180
-    img_width = 180
+    # Use the top Emotion() function to call for the dominant emotion in the image
+    dominant_emotion, emotion_score = emo_detector.top_emotion(test_image_one)
+    print(dominant_emotion, emotion_score)
 
-    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir_train,
-        seed=123,
-        validation_split= 0.2,
-        subset= 'training',
-        image_size=(img_height,img_width),
-        batch_size = batch_size
-    )
-
-    print(len(train_ds))
-
-    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-        data_dir_train,
-        seed=123,
-        validation_split= 0.2,
-        subset= 'validation',
-        label_mode='int',
-        image_size=(img_height,img_width),
-        batch_size = batch_size
-    )
-
-    class_names = train_ds.class_names
-    print(class_names)
-
-
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
-
-    from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
-    num_classes = 2
-    model = Sequential([
-                        layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width,3))
-    ])
-    model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',
-                    activation ='relu', input_shape = (180, 180, 32)))
-    model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',
-                    activation ='relu'))
-    model.add(MaxPool2D(pool_size=(2,2)))
-    model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',
-                    activation ='relu'))
-    model.add(MaxPool2D(pool_size=(2,2)))
-    model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',
-                    activation ='relu'))
-    model.add(MaxPool2D(pool_size=(2,2)))
-    model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same',
-                    activation ='relu'))
-    model.add(MaxPool2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
-
-
-    model.add(Flatten())
-    model.add(Dense(num_classes, activation = "softmax"))
-
-    from tensorflow.keras.optimizers import RMSprop
-
-    model.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                metrics=['accuracy'])
-
-    epochs=1
-    history = model.fit(
-    train_ds,
-    validation_data=val_ds,
-    epochs=epochs
-    )
-
-    image = img.resize((180,180))
-    image = np.asarray(image)
-    image = tf.convert_to_tensor(image)
-    image = tf.expand_dims(image, 0)
-    print(image.shape)
-    y = model.predict(image)
-    print(y[0])
-
-    string = str(y[0][0])
-    # string = "hello"
-
-    # acc = history.history['accuracy']
-    return jsonify({'hello': string})
+    return jsonify({'hello': dominant_emotion})
 
 app.run(port=5000)
 
